1890fa85a2d3341bbba207d846885b601a605f71
NabilaVas <122089241+NabilaVas@users.noreply.github.com>
2023-01-25 10:27:03 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
+     System.out.println("Hi"); 
      System.out.println("Thank You");  
     }  
 }  
